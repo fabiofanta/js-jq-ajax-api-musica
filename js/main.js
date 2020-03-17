606d0a3aad2716ed7eea0b02104571b5d6e827b4
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-03-17 16:59:57 +0100

    1-rel

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
     var source = $("#card-template").html();
     var cardTemplate = Handlebars.compile(source);
-    $('.container').append(cardTemplate);
-    // var context = { title: "My New Post", body: "This is my first post!" };
-    // var html = template(context);
 
+    $.ajax ({
+        url:'https://flynn.boolean.careers/exercises/api/array/music',
+        method:'GET',
+        success: function(data) {
+            var albums = data.response;
+            // console.log(album);
+            cardGenerator(albums);
+        },
 
-})
+        error: function() {
+
+        }
+    });
+
+    function cardGenerator(array) {
+        for (var i = 0; i < array.length; i++) {
+            var album = { albumPic:array[i].poster, albumTitle:array[i].title ,artistName:array[i].author,year:array[i].year,genre:array[i].genre };
+            var filledTemplate = cardTemplate(album);
+            $('.container').append(filledTemplate);
+        };
+    };
+});
